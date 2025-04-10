47317db1133cc3f595a47ae48e81a0b0311e7c31
DropItLikeItsLucy <zuri.mamulashvili@gmail.com>
2025-04-10 23:48:56 +0400

    update page.js

---
+++ app/page.js
@@ -0,0 +1,8 @@
+// Example in app/page.js (Homepage)
+export async function generateMetadata() {
+    return {
+      title: 'Snacky Peanut Butter - Delicious & Healthy',
+      description: 'Buy Snacky peanut butter online or find it in stores near you. Check out our delivery partners.',
+      // Add other relevant meta tags like openGraph
+    };
+  }
